070ec0946f28eae080afef02ea175913fb6e7e4a
_Rroyo65_ <robertoroyomartinez2007@gmail.com>
2024-11-25 00:22:41 +0100

    V4

--- src/RP/Games/Games.java
+++ src/RP/Games/Games.java
@@ -1,10 +1,16 @@
 package RP.Games;
 
+import RP.Games.TicTacToe.TicTacToe;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Games extends JFrame{
     public Games(JPanel Games_, JPanel MM){
+
+        final int[] CountTTT_3 = {0};
+
+        JFrame PanelTTT_3 = new JFrame();
 
         /* Title & Return to Main Menu */{
             JPanel GTitle = new JPanel();
@@ -33,6 +39,27 @@ public class Games extends JFrame{
             });
             GTitle.add(MMenu);
         }
+
+        JLabel TTT_3Title = new JLabel();
+        TTT_3Title.setBounds(10, 80, 100, 20);
+        TTT_3Title.setText("Tic Tac Toe");
+        Games_.add(TTT_3Title);
+
+        JButton TTT_3Button = new JButton();
+        TTT_3Button.setBounds(120, 65, 40, 40);
+        TTT_3Button.setBackground(new Color(255,255,255));
+        TTT_3Button.setBorder(BorderFactory.createLineBorder(Color.black, 1, false));
+        TTT_3Button.addActionListener(_ -> {
+            TicTacToe TTT_3 = new TicTacToe(PanelTTT_3);
+            if (CountTTT_3[0] == 0){
+                TTT_3.Visible(true);
+                CountTTT_3[0]++;
+            } else if (CountTTT_3[0] == 1) {
+                TTT_3.Visible(false);
+                CountTTT_3[0]--;
+            }
+        });
+        Games_.add(TTT_3Button);
 
     }
 }
---
+++ src/RP/Games/TicTacToe/TicTacToe.java
@@ -0,0 +1,53 @@
+package RP.Games.TicTacToe;
+
+import RP.Var.Var;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class TicTacToe {
+	Var var = new Var();
+
+	ArrayList<ArrayList<String>> Tablero = new ArrayList<>(){{
+		for (int i = 0; i < 3; i++) {
+			add(new ArrayList<>(){{
+				for (int j = 0; j < 3; j++) {
+					add(" ");
+				}
+			}});
+		}
+	}};
+
+	private final JFrame TTT;
+
+	public void Visible(Boolean Visible){
+		this.TTT.setVisible(Visible);
+		this.TTT.setBounds(0,0, var.Width(500), var.Height(500));
+		this.TTT.setLocationRelativeTo(null);
+	}
+
+	public TicTacToe(JFrame Panel){
+		this.TTT = Panel;
+		this.TTT.setLayout(null);
+
+		JLabel[] PosicionesTablero = new JLabel[9];
+
+		for (int i = 0; i < PosicionesTablero.length; i++) {
+			PosicionesTablero[i] = new JLabel("", SwingConstants.CENTER);
+			PosicionesTablero[i].setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, false));
+		}
+		for (int i = 300, C = 0; i >= 100; i-=100) {
+			for (int j = 100; j <= 300; j+=100) {
+				PosicionesTablero[C].setBounds(j,i,100,100);
+				C++;
+			}
+		}
+		for (JLabel Pos : PosicionesTablero){
+			this.TTT.add(Pos);
+		}
+
+		this.TTT.setTitle("Tic Tac Toe");
+		this.TTT.setResizable(false);
+	}
+}
